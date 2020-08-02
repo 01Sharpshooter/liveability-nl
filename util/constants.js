46dbb5e458fbbbb79a412efc748fc7aee08a4d31
Soma Holler <soma.holler@gmail.com>
2020-08-02 13:59:32 +0200

    Constants extracted

---
+++ util/constants.js
@@ -0,0 +1,46 @@
+const CSVColumns = Object.freeze({
+    SCORE18: "KL18",
+    DEVELOPMENT1418: "VKL1418",
+    HOUSES12: "RLBWON12",
+    RESIDENTS12: "RLBBEV12",
+    SERVICES12: "RLBVRZ12",
+    SAFETY12: "RLBVEI12",
+    ENVIRONMENT12: "RLBFYS12",
+    HOUSES14: "RLBWON14",
+    RESIDENTS14: "RLBBEV14",
+    SERVICES14: "RLBVRZ14",
+    SAFETY14: "RLBVEI14",
+    ENVIRONMENT14: "RLBFYS14",
+    HOUSES16: "RLBWON16",
+    RESIDENTS16: "RLBBEV16",
+    SERVICES16: "RLBVRZ16",
+    SAFETY16: "RLBVEI16",
+    ENVIRONMENT16: "RLBFYS16",
+    HOUSES18: "RLBWON18",
+    RESIDENTS18: "RLBBEV18",
+    SERVICES18: "RLBVRZ18",
+    SAFETY18: "RLBVEI18",
+    ENVIRONMENT18: "RLBFYS18"
+  });
+  
+  const LiveabilityScoreMeaning = Object.freeze({
+    1: "Lowest",
+    2: "Very Low",
+    3: "Low",
+    4: "Okay",
+    5: "Mostly Sufficient",
+    6: "Sufficient",
+    7: "Good",
+    8: "Very Good",
+    9: "Exceptional"
+  });
+  
+  const DevelopmentScoreMeaning = Object.freeze({
+    1: "Big Decline",
+    2: "Decline",
+    3: "Small Decline",
+    4: "No Changes",
+    5: "Small Progress",
+    6: "Progress",
+    7: "Huge Progress"
+  });
