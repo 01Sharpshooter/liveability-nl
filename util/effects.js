0407f0e1c60c4dbed2f8d04b8043d81ca9174655
Soma Holler <soma.holler@gmail.com>
2020-08-10 12:05:35 +0200

    Ripple remove fix

--- util/effects.js
+++ util/effects.js
@@ -3,7 +3,7 @@ const addRippleOnClick = (node) => {
     elem.addClass("clickable-ripple");
     elem.on("click", (evt) => {
         const rippleEffect = evt.currentTarget.getElementsByClassName("ripple-effect")[0];
-        if (rippleEffect) event.currentTarget.removeChild(rippleEffect);
+        if (rippleEffect) evt.currentTarget.removeChild(rippleEffect);
 
         const eventTarget = $(evt.currentTarget);
         const x = evt.pageX - eventTarget.offset().left;
