53459249509352f528b25a670e6b0a5385730c05
Soma Holler <soma.holler@gmail.com>
2020-08-14 11:24:48 +0200

    Liveability range added to popup

--- popup/popup.js
+++ popup/popup.js
@@ -13,3 +13,26 @@ chrome.storage.local.get(AppSettings.LIVEABILITY_REGIONS_ENABLED, (result) => {
 chkEnabled.addEventListener("change", () => {
     toggleEnable(chkEnabled.checked);
 });
+
+const rangeSlider = () => {
+    const slider = document.getElementById("liveability-range-slider");
+
+    const range = slider.querySelector(".range-slider-range");
+    const span = slider.querySelector(".range-slider-value");
+    const max = Object.keys(LiveabilityClasses).length;
+
+    range.max = max;
+
+    span.innerHTML = LiveabilityClasses[range.value].meaning;
+
+    range.addEventListener("input", (event) => {
+        const element = event.currentTarget;
+        const valueInPercentage = (element.value / element.max * 100) - 1;
+        const backgroundColor = LiveabilityClasses[element.value].backgroundColor;
+        span.innerHTML = LiveabilityClasses[element.value].meaning;
+
+        range.style.background = `linear-gradient(to right, ${backgroundColor} 0% ${valueInPercentage}%, #ccc ${valueInPercentage}% 100%)`;
+    });
+};
+
+rangeSlider();
