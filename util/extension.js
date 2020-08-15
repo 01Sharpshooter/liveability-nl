02f6af368dee8cbed8e54bf2ade670fbb8effe20
Soma Holler <soma.holler@gmail.com>
2020-08-15 22:00:46 +0200

    New method to fetch resources

--- util/extension.js
+++ util/extension.js
@@ -7,6 +7,13 @@ const getResourceURL = (shortName) => {
     }
 }
 
+const getResource = async (shortName) => {
+    const url = getResourceURL(shortName);
+    const response = await fetch(url);
+
+    return response.text();    
+}
+
 const sendMessage = (message) => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
         chrome.tabs.sendMessage(tab[0].id, message);
