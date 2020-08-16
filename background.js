c5a172691c9ed5a371e58ff168bd06078632e818
Soma Holler <soma.holler@gmail.com>
2020-08-16 23:55:34 +0200

    Default development filter

--- background.js
+++ background.js
@@ -2,5 +2,6 @@ chrome.runtime.onInstalled.addListener((details) => {
     if(details.reason == "install"){
         chrome.storage.local.set({ [AppSettings.LIVEABILITY_REGIONS_ENABLED]: true });
         chrome.storage.local.set({ [AppSettings.MIN_LIVEABILITY_SCORE]: 1 });
+        chrome.storage.local.set({ [AppSettings.MIN_DEVELOPMENT_SCORE]: 1 });
     }
 })
