d27910ebef93365cc5fb06ed359da22f0bdfc902
Soma Holler <soma.holler@gmail.com>
2020-08-16 23:40:34 +0200

    Read local storage with Promise

--- util/extension.js
+++ util/extension.js
@@ -11,11 +11,19 @@ const getResource = async (shortName) => {
     const url = getResourceURL(shortName);
     const response = await fetch(url);
 
-    return response.text();    
+    return response.text();
 }
 
 const sendMessage = (message) => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
         chrome.tabs.sendMessage(tab[0].id, message);
+    });
+}
+
+const readLocalSetting = (key) => {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(key, (result) => {
+            resolve(result[key]);
+        });
     });
 }
