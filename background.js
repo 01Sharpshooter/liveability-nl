36cc572b76c4e98c6f9b86a4708d3300a445fcbf
Soma Holler <soma.holler@gmail.com>
2020-08-18 11:32:38 +0200

    More data init on installation

--- background.js
+++ background.js
@@ -3,5 +3,8 @@ chrome.runtime.onInstalled.addListener((details) => {
         chrome.storage.local.set({ [AppSettings.LIVEABILITY_REGIONS_ENABLED]: true });
         chrome.storage.local.set({ [AppSettings.MIN_LIVEABILITY_SCORE]: 1 });
         chrome.storage.local.set({ [AppSettings.MIN_DEVELOPMENT_SCORE]: 1 });
+        chrome.storage.local.set({ [AppSettings.MIN_HOUSES_SCORE]: 1 });
+        chrome.storage.local.set({ [AppSettings.MIN_SERVICES_SCORE]: 1 });
+        chrome.storage.local.set({ [AppSettings.MIN_SAFETY_SCORE]: 1 });
     }
 })
